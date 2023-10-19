c0e17ecb1ec48253b9960d5184f177824a954ca7
LuisHGonz94 <drums4gusto@gmail.com>
2023-10-19 19:17:23 +0200

    problema resuelto

--- index.py
+++ index.py
@@ -79,4 +79,16 @@ story2 = story.replace('Javier', 'pepito') # write your code here
 '''
 Exercise 10:** Reverse the order of the words in the story.
 '''
+story3 = story.split() # write your code here
+# print("Story with words reversed:", story3[::-1])
 
+'''
+Exercise 11:** write story to a file with a parameterized name. 
+'''
+
+def writeFile(story, file_name):
+  with open(file_name, "w") as f:
+    f.write(story)
+
+writeFile(story2, "story-pepito.txt")
+writeFile(story3, "story-reversed.txt")
