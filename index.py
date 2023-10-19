6570107ebd1b77ceb234f5fa045a33d02215111a
LuisGonzH94 <drums4gusto@gmail.com>
2023-10-19 12:20:03 +0200

    Exercise 3 Solution

--- index.py
+++ index.py
@@ -32,8 +32,7 @@ print("Number of years left:", years_left)
 '''
 Exercise 3:** Calculate the number of DAYS until 2045.
 '''
-days_left = 0 # write your code here
-total_days = 365
+days_left = new_year * 365 # write your code here
 print("Number of days left:", days_left)
 
 # 3. Booleans
